62308e6f7ae8f6cfffc0d5dde3a2e0e96eab024a
Marko Galjak <marko@catalystbalkans.org>
2026-03-11 07:24:21 +0100

    bibtex bug2

--- _plugins/hideCustomBibtex.rb
+++ _plugins/hideCustomBibtex.rb
@@ -1,6 +1,7 @@
  module Jekyll
   module HideCustomBibtex
     def hideCustomBibtex(input)
+      return input if input.nil?
 	  keywords = @context.registers[:site].config['filtered_bibtex_keywords']
 
 	  keywords.each do |keyword|
