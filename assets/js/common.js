3a2ef8ee558bf61e299d92c10db97608108caa6e
Marko Galjak <marko@catalystbalkans.org>
2026-09-04 06:55:34 +0200

    added repos

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    $('a.abstract').click(function(event) {
+        event.preventDefault();
+
+        var abstract = $(this).closest('.publication-entry').find('div.abstract.hidden').first();
+        abstract.toggleClass('open');
+        $(this).attr('aria-expanded', abstract.hasClass('open'));
     });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    $('a.bibtex').click(function(event) {
+        event.preventDefault();
+
+        var bibtex = $(this).closest('.publication-entry').find('div.bibtex.hidden').first();
+        bibtex.toggleClass('open');
+        $(this).attr('aria-expanded', bibtex.hasClass('open'));
     });
     $('a').removeClass('waves-effect waves-light');
 });
