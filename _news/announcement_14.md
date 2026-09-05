071bdc722ed736d28bc513cd8a8c5b1145c347aa
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-05 18:34:20 +0000

    added theses and changed cv page [ci skip]

--- _news/announcement_14.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 2026-06-15 15:59:00-0400
-inline: true
----
-
-Received a fast-track promotion to Senior Research Associate.
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,17 +1,1 @@
-$(document).ready(function() {
-    $('a.abstract').click(function(event) {
-        event.preventDefault();
-
-        var abstract = $(this).closest('.publication-entry').find('div.abstract.hidden').first();
-        abstract.toggleClass('open');
-        $(this).attr('aria-expanded', abstract.hasClass('open'));
-    });
-    $('a.bibtex').click(function(event) {
-        event.preventDefault();
-
-        var bibtex = $(this).closest('.publication-entry').find('div.bibtex.hidden').first();
-        bibtex.toggleClass('open');
-        $(this).attr('aria-expanded', bibtex.hasClass('open'));
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
+$(document).ready(function(){$("a.abstract").click(function(t){t.preventDefault();var e=$(this).closest(".publication-entry").find("div.abstract.hidden").first();e.toggleClass("open"),$(this).attr("aria-expanded",e.hasClass("open"))}),$("a.bibtex").click(function(t){t.preventDefault();var e=$(this).closest(".publication-entry").find("div.bibtex.hidden").first();e.toggleClass("open"),$(this).attr("aria-expanded",e.hasClass("open"))}),$("a").removeClass("waves-effect waves-light")});
